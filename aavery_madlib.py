32aaace177742e341e77f6434ca52ee666a8572f
ausAvery <austin.avery4@gmail.com>
2024-05-17 01:16:07 -0400

    Input and Variables Module

---
+++ aavery_madlib.py
@@ -0,0 +1,23 @@
+# Austin Avery
+# Mad Lib
+# Wednesday 15, May 2024
+# Mad Lib game with lyrics from Bohemian Rhapsody
+
+# Input - variables
+
+descriptiveNoun = input("Please give me an describing word. ")
+geographicalDisaster = input("Please give me a natural disaster. ")
+bodyPart = input("Please give me a plural body part. ")
+actionVerb = input("Please give me a action verb. ")
+adjective = input("Please give me a word to describe a person. ")
+gender = input("Please specify a gender. ")
+parentFigure = input("Please give me a parent figure (ex. Mother, Father, Mama, Papa, etc...). ")
+element = input("Please give me an element. ")
+pronoun = input("Please give me a pronoun. ")
+adverb = input("Please give me an adverb. ")
+emotion = input("Please give me an emotion. ")
+weekday = input("Please give any day of the week. ")
+
+# Output - Bohemian Rhapsody Lyrics w/ variables
+
+print(f"Is this the real life? Is this just {descriptiveNoun}?\nCaught in a {geographicalDisaster}, no escape from reality\nOpen your {bodyPart}, {actionVerb} up to the skies and see\nI'm just a {adjective} {gender}, I need no sympathy\nBecause I'm easy come, easy go, little high, little low\nAny way the {element} blows doesn't really matter to me, to me\n{parentFigure}, just killed a man\nPut a gun against his head, pulled my trigger, now {pronoun}'s dead\n{parentFigure}, life had just begun\nBut now I've gone and thrown it all {adverb}\n{parentFigure}, ooh, didn't mean to make you {emotion}\nIf I'm not back again this time {weekday}\nCarry on, carry on as if nothing really matters")
