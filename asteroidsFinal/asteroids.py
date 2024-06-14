3d90cf3a3333c9d3387f32f1e1cc94b22025b138
ausAvery <austin.avery4@gmail.com>
2024-06-13 20:51:33 -0400

    asteroidsFinal UPDATE

--- asteroidsFinal/asteroids.py
+++ asteroidsFinal/asteroids.py
@@ -12,6 +12,7 @@ class Game(simpleGE.Scene):
     
     def __init__(self, size = (720, 480)):
         super().__init__()
+        
         self.screen = pygame.display.set_mode(size)
         self.background = pygame.Surface(self.screen.get_size())
         self.setCaption("Asteroids")
@@ -22,15 +23,35 @@ class Game(simpleGE.Scene):
         
         self.asteroidSm = AsteroidSm(self)
         
-        self.bullet = Bullet(self)
+#         self.bullet = Bullet(self, self.spaceship)
+
+        #what i think bullet will become
+        self.bullet = 0
+        self.bullets = []
+        self.numBullets = 100
+        for i in range(self.numBullets):
+            self.bullets.append(Bullet(self, self.spaceship))
         
         self.sprites = [self.spaceship,
                         self.asteroidLg,
                         self.asteroidSm,
-                        self.bullet]
+                        self.bullets]
         
+    def processEvent(self, event):
+        
+        if self.isKeyPressed(pygame.K_SPACE):
+            self.bullet += 1
+            if self.bullet >= self.numBullets:
+                self.bullet = 0
+            self.bullets[self.bullet].fire()
+            
     def process(self):
-        
+        if self.isKeyPressed(pygame.K_s):
+            self.bullet += 1
+            if self.bullet >= self.numBullets:
+                self.bullet = 0
+            self.bullets[self.bullet].fire()
+            
         
 class Spaceship(simpleGE.Sprite):
     
@@ -41,7 +62,6 @@ class Spaceship(simpleGE.Sprite):
         self.position = (360, 240)
         self.setSize(50,30)
         self.imageAngle = 90
-        self.speed = 0
         self.drag = .05
         self.accel = .2
 
@@ -54,9 +74,12 @@ class Spaceship(simpleGE.Sprite):
             self.imageAngle -= 5
         if self.isKeyPressed(pygame.K_UP):
             self.addForce(self.accel, self.imageAngle)
-
+            
         # bullet shooting mechanism
-        if self.isKeyPressed(pygame.K_SPACE):
+#         if self.isKeyPressed(pygame.K_SPACE):
+
+        # compensate for drag (DOES NOT WORK)
+#         self.speed *= (1 - self.drag)
             
 
 class AsteroidLg(simpleGE.Sprite):
@@ -72,7 +95,7 @@ class AsteroidLg(simpleGE.Sprite):
         x = random.randint(0, self.screen.get_width())
         y = random.randint(0, self.screen.get_height())
         self.position = (x, y)
-        self.dx = random.randint(-3, 8)
+        self.dx = random.randint(-5, 5)
         self.dy = random.randint(-5, 5)
     
 #     def checkBounds(self):
@@ -85,6 +108,7 @@ class AsteroidSm(simpleGE.Sprite):
         self.setImage("asteroidSm.png")
         self.setSize(60, 40)
         self.reset()
+        self.hide()
     
     def reset(self):
         x = random.randint(0, self.screen.get_width())
@@ -95,12 +119,24 @@ class AsteroidSm(simpleGE.Sprite):
         
 class Bullet(simpleGE.Sprite):
     
-    def __init__(self, scene):
+    def __init__(self, scene, parent):
         super().__init__(scene)
+        self.parent = parent
         
-        self.setImage("bullet.png")
-        self.setSize(100, 100)
+#         self.setImage("bullet.png")
+#         self.setSize(100, 100)
+        self.colorRect("sky blue", (8,3))
+        self.boundAction = self.HIDE
+#         self.setBoundAction(self.HIDE)
+        self.hide()
         
+    def fire(self):
+#         if not self.visible:
+        self.show()
+        self.position = self.parent.position
+        self.imageAngle = self.parent.imageAngle
+        self.moveAngle = self.parent.imageAngle
+        self.speed = 15        
         
 def main():
     game = Game()
